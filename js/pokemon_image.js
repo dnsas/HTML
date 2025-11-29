02aefb05d20d35ca6233040562db6d79a66ae287
omer <saifoulayediallo250@gmail.com>
2025-11-30 00:41:25 +0100

    Fix: remove submodule and add js as normal folder

---
+++ js/pokemon_image.js
@@ -0,0 +1,31 @@
+// Affichage des images Pokémon
+console.log("Chargement du module images Pokémon");
+
+// Surcharger la fonction d'affichage
+const originalAfficher = window.afficherResultatsRequete;
+window.afficherResultatsRequete = function(enregistrements) {
+    originalAfficher.call(this, enregistrements);
+    ajouterImagesPokemon();
+};
+
+// Ajouter les images dans le tab
+function ajouterImagesPokemon() {
+    const tbody = document.querySelector('.affichageBD tbody');
+    if (!tbody) return;
+
+    tbody.querySelectorAll('tr').forEach(ligne => {
+        const cellules = ligne.querySelectorAll('td');
+
+        // Chercher un ID numérique
+        cellules.forEach(cellule => {
+            const texte = cellule.textContent.trim();
+            if (/^\d+$/.test(texte)) {
+                const img = document.createElement('img');
+                img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${texte}.png`;
+                img.alt = `Pokémon ${texte}`;
+                img.classList.add('pokemon-image');
+                cellule.prepend(img);
+            }
+        });
+    });
+}
